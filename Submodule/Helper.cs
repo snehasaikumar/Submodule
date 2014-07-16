d01e72b229682a5310e139682312afe968316844
Sneha Saikumar <ssaikumar@hhogdev.com>
2014-07-16 17:30:53 -0400

    Initial commit

---
+++ Submodule/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Submodule
+{
+    public class Helper
+    {
+
+        public string GetStatus()
+        {
+            return "I love nature :) ";
+        }
+
+    }
+}
